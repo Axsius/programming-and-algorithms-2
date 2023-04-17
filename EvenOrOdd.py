b8380de861387ba219738dcff2325bfae899f5b0
Axsius <106613727+Axsius@users.noreply.github.com>
2023-04-17 07:31:17 +0545

    even odd, SI, If statement

---
+++ EvenOrOdd.py
@@ -0,0 +1,5 @@
+num= int(input("eneter a number: "))
+if num % 2 == 0:
+    print(num, "is even")
+else:
+    print(num, "is odd")
---
+++ if-statement.py
@@ -0,0 +1,9 @@
+age = int(input("eneter a your: "))
+if age < 10 :
+    print("your are childern")
+elif age >= 11 and age <= 20: 
+    print( "Your are teen")
+elif age >= 21 and age <= 50: 
+    print( "Your are adult") 
+elif age > 50:
+    print( "Your are old")

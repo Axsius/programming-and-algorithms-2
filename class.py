b8db11ed34d21539e8247a4ca420fd839868d08c
Axsius <106613727+Axsius@users.noreply.github.com>
2023-04-19 07:59:03 +0545

    cw

---
+++ class.py
@@ -0,0 +1,8 @@
+fruits = ["apple", "banana", "orange"]
+count_apple = fruits.count("apple")
+print("The number of times 'apple' appears in the list is:", count_apple)
+a = "Hello world"
+b= len(a)
+c= a.casefold()
+print ("the length ", b)
+print ("string in lower case ", c)
